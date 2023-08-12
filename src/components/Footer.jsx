559f9d451d7ae01be8532341e3031dea5cf2c23b
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-08-12 00:03:11 -0500

    react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Mariscal Castilla. Todos los derechos reservados.</p>
-        <p>Contacto: <a href="mailto:concact@mariscalcastilla.edu.pe">contact@mariscalcastilla.edu.pe</a></p>
-      </div>
-    </footer>
+    return (
+        <>
+        <div>
+            <footer className="footer">
+              <div className="footer-content">
+                <p>&copy; {new Date().getFullYear()} Mariscal Castilla. Todos los derechos reservados.</p>
+                <p>Contacto: <a href="mailto:concact@mariscalcastilla.edu.pe">contact@mariscalcastilla.edu.pe</a></p>
+              </div>
+            </footer>
+        </div>
+        </>
   );
 }
 
