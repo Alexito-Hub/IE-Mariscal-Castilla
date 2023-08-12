d3f5ea255ec5543bb364e5281f034be14a0016aa
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-08-12 08:54:46 -0500

    d

--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -4,26 +4,31 @@ import './css/Welcome.css'
 
 function Welcome() {
     return (
-    <>
-    <div className="welcome-container">
-      <div className="logo-container">
-        <img src="#" alt="Logo de Mariscal Castilla" className="logo" />
-        <h1>Mariscal Castilla</h1>
-      </div>
-      <div className="welcome-message">
-        <h2>Bienvenido al sitio web de la Institución Educativa Mariscal Castilla</h2>
-        <p>Por favor, inicia sesión para acceder al sitio.</p>
-        <Link to="/login">
-          <button>Iniciar sesión</button>
-        </Link>
-        <p>O</p>
-        <Link to="/inicio">
-          <button>Continuar sin iniciar sesión</button>
-        </Link>
-      </div>
-    </div>
-    </>
-  );
+        <>
+        <div className="welcome-container">
+        
+            <div className="logo-container">
+                <h1>Mariscal Castilla</h1>
+            </div>
+            
+            <div className="welcome-message">
+                <h2>Bienvenido al sitio web de la Institución Educativa Mariscal Castilla</h2>
+                <p>Por favor, inicia sesión para acceder al sitio.</p>
+                
+                <Link to="/login">
+                    <button>Iniciar sesión</button>
+                </Link>
+                
+                <p>O</p>
+                
+                <Link to="/inicio">
+                    <button>Continuar sin iniciar sesión</button>
+                </Link>
+                
+            </div>
+        </div>
+        </>
+    );
 }
 
 export default Welcome
