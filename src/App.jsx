5c0a745b5bc05aeca063f16a47e36a1ef070bcfc
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-08-11 21:42:41 -0500

    b

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
         <MenuBar />
         <div style={{ padding: "20px" }}>
           <h1>Bienvenido a la Institución Educativa</h1>
+          <h2>Mariscal Castilla</h2>
           <p>Explora nuestros cursos, participa en eventos y contáctanos para más información.</p>
         </div>
       </div>
