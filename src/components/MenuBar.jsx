68ec87ff061a4523b2506c0592f51332943573d4
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-08-11 20:41:25 -0500

    new

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -4,50 +4,52 @@ import { ReactComponent as Logo } from "../assets/logo.svg";
 import "./MenuBar.css";
 
 function MenuBar() {
-  const [showMenu, setShowMenu] = useState(false);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
-  return (
-    <nav className="navbar">
-      <div>
-        <Logo className="logo" />
-        <span>Institución Educativa Mariscal Castilla</span>
-      </div>
-      <div>
-        <a href="#inicio">Inicio</a>
-        <a href="#cursos">Cursos</a>
-        <a href="#eventos">Eventos</a>
-        <a href="#contacto">Contacto</a>
-        <a href="#login">
-          <img src="#" alt="Login" />
-          Login
-        </a>
-        <a href="#chat">
-          <img src="#" alt="Chat" />
-          Chat
-        </a>
-        <span className="menu-icon" onClick={toggleMenu}>
-          &#9776;
-        </span>
-        <div className={`menu ${showMenu ? "show" : ""}`}>
-          <a href="#inicio">Inicio</a>
-          <a href="#cursos">Cursos</a>
-          <a href="#eventos">Eventos</a>
-          <a href="#contacto">Contacto</a>
-          <a href="#login">
-            <img src="#" alt="Login" />
-            Login
-          </a>
-          <a href="#chat">
-            <img src="#" alt="Chat" />
-            Chat
-          </a>
-        </div>
-      </div>
-    </nav>
+    const [showMenu, setShowMenu] = useState(false);
+    
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+    
+    return (
+        <>
+        <nav className="navbar">
+          <div>
+            <Logo className="logo" />
+            <span>Institución Educativa Mariscal Castilla</span>
+          </div>
+          <div>
+            <a href="#inicio">Inicio</a>
+            <a href="#cursos">Cursos</a>
+            <a href="#eventos">Eventos</a>
+            <a href="#contacto">Contacto</a>
+            <a href="#login">
+              <img src="#" alt="Login" />
+              Login
+            </a>
+            <a href="#chat">
+              <img src="#" alt="Chat" />
+              Chat
+            </a>
+            <span className="menu-icon" onClick={toggleMenu}>
+              &#9776;
+            </span>
+            <div className={`menu ${showMenu ? "show" : ""}`}>
+              <a href="#inicio">Inicio</a>
+              <a href="#cursos">Cursos</a>
+              <a href="#eventos">Eventos</a>
+              <a href="#contacto">Contacto</a>
+              <a href="#login">
+                <img src="#" alt="Login" />
+                Login
+              </a>
+              <a href="#chat">
+                <img src="#" alt="Chat" />
+                Chat
+              </a>
+            </div>
+          </div>
+        </nav>
+        </>
   );
 }
 
