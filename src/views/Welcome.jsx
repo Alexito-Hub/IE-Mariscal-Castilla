ad0ff4bc866878fe2af4d541579ed1f4ce5c3d67
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-08-12 03:14:12 -0500

    u

--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png'
-import './src/Welcome.css'
+import './css/Welcome.css'
 
 function Welcome() {
     return (
